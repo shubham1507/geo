2311f8a8d1e92e5a12a0ce272a2617aa45401c71
shubham1507 <shubham.joshi1507@gmail.com>
2019-01-24 12:51:20 +0530

    Can now retrieve search results

--- gigs/tests.py
+++ gigs/tests.py
@@ -71,3 +71,39 @@ class LookupViewTest(TestCase):
         response = LookupView.as_view()(request)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('gigs/lookup.html')
+
+    def test_post(self):
+        # Create venues to return
+        v1 = VenueFactory(name='Venue1')
+        v2 = VenueFactory(name='Venue2')
+        v3 = VenueFactory(name='Venue3')
+        v4 = VenueFactory(name='Venue4')
+        v5 = VenueFactory(name='Venue5')
+        v6 = VenueFactory(name='Venue6')
+        v7 = VenueFactory(name='Venue7')
+        v8 = VenueFactory(name='Venue8')
+        v9 = VenueFactory(name='Venue9')
+        v10 = VenueFactory(name='Venue10')
+        # Create events to return
+        e1 = EventFactory(name='Event1', venue=v1)
+        e2 = EventFactory(name='Event2', venue=v2)
+        e3 = EventFactory(name='Event3', venue=v3)
+        e4 = EventFactory(name='Event4', venue=v4)
+        e5 = EventFactory(name='Event5', venue=v5)
+        e6 = EventFactory(name='Event6', venue=v6)
+        e7 = EventFactory(name='Event7', venue=v7)
+        e8 = EventFactory(name='Event8', venue=v8)
+        e9 = EventFactory(name='Event9', venue=v9)
+        e10 = EventFactory(name='Event10', venue=v10)
+        # Set parameters
+        lat = 52.3749159
+        lon = 1.1067473
+        # Put together request
+        data = {
+            'latitude': lat,
+            'longitude': lon
+        }
+        request = self.factory.post(reverse('lookup'), data)
+        response = LookupView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('gigs/lookupresults.html')
--- gigs/views.py
+++ gigs/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-
-
-class LookupView(View):
-   def get(self, request):
-        return render_to_response('gigs/lookup.html')
+from django.shortcuts import render_to_response
+from django.views.generic.edit import FormView
+from gigs.forms import LookupForm
+from gigs.models import Event
+from django.utils import timezone
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from django.template import RequestContext
+class LookupView(FormView):
+    form_class = LookupForm
+    def get(self, request):
+        return render_to_response('gigs/lookup.html', RequestContext(request))
+    def form_valid(self, form):
+        # Get data
+        latitude = form.cleaned_data['latitude']
+        longitude = form.cleaned_data['longitude']
+        # Get today's date
+        now = timezone.now()
+        # Get next week's date
+        next_week = now + timezone.timedelta(weeks=1)
+        # Get Point
+        location = Point(longitude, latitude, srid=4326)
+        # Look up events
+        events = Event.objects.filter(datetime__gte=now).filter(datetime__lte=next_week).annotate(distance=Distance('venue__location', location)).order_by('distance')[0:5]
+        # Render the template
+        return render_to_response('gigs/lookupresults.html', {
+            'events': events
+            })
