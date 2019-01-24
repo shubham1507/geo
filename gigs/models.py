dc5bd22bab8160f4c06ea40b953dcb55d514cacd
shubham1507 <shubham.joshi1507@gmail.com>
2019-01-24 11:36:04 +0530

    Venue model in place

--- gigs/models.py
+++ gigs/models.py
@@ -1,3 +1,7 @@
-from django.db import models
-
-# Create your models here.
+from django.contrib.gis.db import models
+class Venue(models.Model):
+    """
+    Model for a venue
+    """
+    name = models.CharField(max_length=200)
+    location = models.PointField()
