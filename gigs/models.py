62cd402a73e6595c58a91632b9561c272523332f
shubham1507 <shubham.joshi1507@gmail.com>
2019-01-24 11:55:58 +0530

    Added Event model

--- gigs/models.py
+++ gigs/models.py
@@ -1,7 +1,16 @@
 from django.contrib.gis.db import models
+
 class Venue(models.Model):
     """
     Model for a venue
     """
     name = models.CharField(max_length=200)
     location = models.PointField()
+
+class Event(models.Model):
+    """
+    Model for an event
+    """
+    name = models.CharField(max_length=200)
+    datetime = models.DateTimeField()
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
--- gigs/tests.py
+++ gigs/tests.py
@@ -1,17 +1,14 @@
 from django.test import TestCase
-from gigs.models import Venue
+from gigs.models import Venue, Event
 from factory.fuzzy import BaseFuzzyAttribute
 from django.contrib.gis.geos import Point
 import factory.django, random
-
-
+from django.utils import timezone
 class FuzzyPoint(BaseFuzzyAttribute):
     def fuzz(self):
         return Point(random.uniform(-180.0, 180.0),
                      random.uniform(-90.0, 90.0))
 # Factories for tests
-
-
 class VenueFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Venue
@@ -21,8 +18,16 @@ class VenueFactory(factory.django.DjangoModelFactory):
         )
     name = 'Wembley Arena'
     location = FuzzyPoint()
-
-    
+class EventFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Event
+        django_get_or_create = (
+            'name',
+            'venue',
+            'datetime'
+        )
+    name = 'Queens of the Stone Age'
+    datetime = timezone.now()
 class VenueTest(TestCase):
     def test_create_venue(self):
         # Create the venue
@@ -34,3 +39,17 @@ class VenueTest(TestCase):
         self.assertEqual(only_venue, venue)
         # Check attributes
         self.assertEqual(only_venue.name, 'Wembley Arena')
+class EventTest(TestCase):
+    def test_create_event(self):
+        # Create the venue
+        venue = VenueFactory()
+        # Create the event
+        event = EventFactory(venue=venue)
+        # Check we can find it
+        all_events = Event.objects.all()
+        self.assertEqual(len(all_events), 1)
+        only_event = all_events[0]
+        self.assertEqual(only_event, event)
+        # Check attributes
+        self.assertEqual(only_event.name, 'Queens of the Stone Age')
+        self.assertEqual(only_event.venue.name, 'Wembley Arena')
