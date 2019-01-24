072887bf0e8c46e097c8924630c0a259bc7534b0
shubham1507 <shubham.joshi1507@gmail.com>
2019-01-24 12:07:13 +0530

    Customised location widget

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from gigs.models import Venue, Event
+from django.forms import ModelForm
+from floppyforms.gis import PointWidget, BaseGMapWidget
+class CustomPointWidget(PointWidget, BaseGMapWidget):
+    class Media:
+        js = ('/static/floppyforms/js/MapWidget.js',)
+class VenueAdminForm(ModelForm):
+    class Meta:
+        model = Venue
+        fields = ['name', 'location']
+        widgets = {
+            'location': CustomPointWidget()
+        }
+class VenueAdmin(admin.ModelAdmin):
+    form = VenueAdminForm
 
-
-admin.site.register(Venue)
+    
+admin.site.register(Venue, VenueAdmin)
 admin.site.register(Event)
